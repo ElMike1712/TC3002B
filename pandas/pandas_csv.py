4df1543e90fffdf5c67b7c43ed6ee7a7d3753850
Mike <miguesv1712@gmail.com>
2023-09-28 14:57:51 -0600

    pandas

--- pandas/pandas_csv.py
+++ pandas/pandas_csv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-datos = pd.read_csv('datos.csv')
+datos = pd.read_csv('pandas/datos.csv')
 print(datos)
 
-wine_reviews = pd.read_csv("winemag-data-130k-v2.csv", index_col=0)
+wine_reviews = pd.read_csv("pandas/winemag-data-130k-v2.csv", index_col=0)
 
 print('Reseñas de vinos')
 print (wine_reviews.shape)
